939543b22bc3cfa6ff6066f7d91e3e8af64af70c
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-31 19:20:25 +0200

    Container
    tzid container for decoration

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     ),
