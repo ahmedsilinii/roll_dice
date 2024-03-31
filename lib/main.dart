7dec5069f2be279ca2ec2daf2f1ae7086df42c17
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-31 19:24:36 +0200

    BoxDecoration
    zedt gradient
    LinearGradient
    begin w end ..

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
-            color: Colors.blue,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 177, 176, 232),
+                Color.fromARGB(255, 121, 120, 168),
+              ],
+            ),
           ),
           child: const Center(
             child: Text('Hello World'),
