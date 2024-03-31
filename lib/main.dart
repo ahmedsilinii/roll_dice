8969b20e44f776198d3df74da20d5cf8e8c93639
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-31 19:27:08 +0200

    text style
    TextStyle in Text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
@@ -16,7 +17,13 @@ void main() {
             ),
           ),
           child: const Center(
-            child: Text('Hello World'),
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
           ),
         ),
       ),
